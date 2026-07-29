0dd8e67592bf80d9dca804b562a0b7924adf0a39
anurag <yourboyanurag777@gmail.com>
2026-07-29 16:52:12 +0530

    fixed es lint

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,11 @@ from pydantic import BaseModel
 app = FastAPI(title="Pipeline Studio API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://vectorshift-assignment-da7y.vercel.app",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
