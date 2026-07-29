9aa88637e0f0ae210f1ae4a5d595d901efee7843
anurag <yourboyanurag777@gmail.com>
2026-07-29 16:31:30 +0530

    Added sh file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,17 @@ set -e
 cd /app
 
 export PORT=${PORT:-80}
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+echo "==> PORT is: $PORT"
 
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+echo "==> Generated nginx config:"
+cat /etc/nginx/conf.d/default.conf
+
+echo "==> Testing nginx config:"
+nginx -t
+
+echo "==> Starting uvicorn:"
 uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
+
+echo "==> Starting nginx:"
 exec nginx -g 'daemon off;'
